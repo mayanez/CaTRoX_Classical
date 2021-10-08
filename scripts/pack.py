21b355ac4b2a19470896eb4900a6a4aba5ac5959
Miguel A. Arroyo <miguel@arroyo.me>
2021-10-08 15:59:37 -0700

    [mod] Update `pack.py`
    * Properly package components
    * Download external packages

--- scripts/pack.py
+++ scripts/pack.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 import argparse
+import os
 import shutil
+import urllib.request
 import zipfile
 from pathlib import Path
 from zipfile import ZipFile
@@ -12,17 +14,16 @@ def path_basename_tuple(path):
     return (path, path.name)
 
 def zipdir(zip_file, path, arc_path=None):
-    assert(path.exists() and path.is_dir())
-
-    for file in path.rglob('*'):
-        if (file.name.startswith('.')):
-            # skip `hidden` files
-            continue
-        if (arc_path):
-            file_arc_path = f'{arc_path}/{file.relative_to(path)}'
-        else:
-            file_arc_path = file.relative_to(path)
-        zip_file.write(file, file_arc_path)
+    if (path.exists() and path.is_dir()):
+        for file in path.rglob('*'):
+            if (file.name.startswith('.')):
+                # skip `hidden` files
+                continue
+            if (arc_path):
+                file_arc_path = f'{arc_path}/{file.relative_to(path)}'
+            else:
+                file_arc_path = file.relative_to(path)
+            zip_file.write(file, file_arc_path)
 
 def pack():
     cur_dir = Path(__file__).parent.absolute()
@@ -31,27 +32,39 @@ def pack():
     output_dir = root_dir/'_result'
     output_dir.mkdir(parents=True, exist_ok=True)
     
-    output_zip = output_dir/'catrox_qwr_edition.zip'
-    output_zip.unlink(missing_ok=True)
+    output_zip = output_dir/'catrox_classical_edition.zip'
+    try:
+        os.remove(output_zip)
+    except:
+        pass
     
     output_packages_dir = output_dir/'packages'
     if output_packages_dir.exists():
         shutil.rmtree(output_packages_dir)
     output_packages_dir.mkdir(parents=True)
     
+    external_packages = {'ArtistBioAlternate': 'https://github.com/mayanez/WilB-Biography-Mod/releases/latest/download/SMP-Biography-Mod.zip', 
+                         'ComposerBio': 'https://github.com/mayanez/WilB-Biography-Mod/releases/latest/download/SMP-Biography-Mod.zip'}
+
     for d in (root_dir/'packages').glob('*'):
-        for f in d.glob('*'):
-            package_zip = output_packages_dir/f'{f.name}.zip'
-            with ZipFile(package_zip, 'w', compression=zipfile.ZIP_DEFLATED, compresslevel=9) as z:
-                zipdir(z, f)   
-            print(f'Generated package: {package_zip}')
-            
-            # there should be only one package per dir anyway
-            break
+        d_name = os.path.basename(d)
+        if d_name not in external_packages.keys():
+            for f in d.glob('*'):
+                package_zip = output_packages_dir/f'{d_name}.zip'
+                with ZipFile(package_zip, 'w', compression=zipfile.ZIP_DEFLATED, compresslevel=9) as z:
+                    zipdir(z, f)   
+                    print(f'Generated package: {package_zip}')
+                
+                # there should be only one package per dir anyway
+                break
+        else:
+            urllib.request.urlretrieve(external_packages[d_name], output_packages_dir/f'{d_name}.zip')
+            print(f'Downloaded package: {d_name}')
 
     with ZipFile(output_zip, 'w', compression=zipfile.ZIP_DEFLATED, compresslevel=9) as z:
         zipdir(z, output_packages_dir, 'packages')
         zipdir(z, root_dir/'fonts', 'fonts')
+        z.write(root_dir/'layout'/'classical.fcl', 'layout/classical.fcl')
         z.write(root_dir/'layout'/'theme.fcl', 'layout/theme.fcl')
 
     print(f'Generated file: {output_zip}')
