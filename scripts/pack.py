888c76141207e3cfed4f3fa52b0885fafb4ebf2e
Miguel A. Arroyo <miguel@arroyo.me>
2021-10-10 11:16:54 -0700

    [mod-scripts] Package Script - Fetch using YAML
    * Download user-component dependencies and save as fb2k-components.
    Allows for easy install & upgrade later.

--- scripts/pack.py
+++ scripts/pack.py
@@ -2,13 +2,19 @@
 
 import argparse
 import os
+import requests
 import shutil
-import urllib.request
+import yaml
 import zipfile
 from pathlib import Path
 from zipfile import ZipFile
 
 import call_wrapper
+
+PACK_YML = 'pack.yml'
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0' ,
+}
 
 def path_basename_tuple(path):
     return (path, path.name)
@@ -24,6 +30,47 @@ def zipdir(zip_file, path, arc_path=None):
             else:
                 file_arc_path = file.relative_to(path)
             zip_file.write(file, file_arc_path)
+
+def handle_package(package_path, output_packages_dir):
+    package_name = os.path.basename(package_path)
+    package_zip = output_packages_dir/f'{package_name}.zip'
+    
+    for f in package_path.glob('*'):
+        if os.path.basename(f) == PACK_YML:
+            with open(f, "r") as ymlfile:
+                data = yaml.load(ymlfile, Loader=yaml.FullLoader)
+                r = requests.get(data[package_name]['url'])
+                with open(package_zip, 'wb') as out:
+                    out.write(r.content)
+                    print(f'Downloaded package: {package_zip}')
+        elif f.is_dir():
+            with ZipFile(package_zip, 'w', compression=zipfile.ZIP_DEFLATED, compresslevel=9) as z:
+                zipdir(z, f)   
+                print(f'Generated package: {package_zip}')
+        
+def handle_additional_files(root_dir, zip_file):
+    for d in (root_dir/'packages').glob('*'):
+        package_name = os.path.basename(d)
+        for f in d.glob('*'):
+            if os.path.basename(f) == PACK_YML:
+                with open(f, "r") as ymlfile:
+                    data = yaml.load(ymlfile, Loader=yaml.FullLoader)
+                    # TODO: Handle arbitrary nesting?
+                    for output_dir, additional_files in data[package_name]['additional_files'].items():
+                        for output_file in additional_files:
+                            zip_file.write(f'{d}/{output_file}', f'{output_dir}/{output_file}')
+
+def handle_user_components(component_path, output_components_dir):
+    component_name = os.path.basename(component_path)
+
+    for f in component_path.glob('*'):
+        if os.path.basename(f) == PACK_YML:
+            with open(f, "r") as ymlfile:
+                data = yaml.load(ymlfile, Loader=yaml.FullLoader)
+                r = requests.get(data[component_name]['url'])
+                with open(f'{output_components_dir}/{component_name}.fb2k-component', 'wb') as out:
+                    out.write(r.content)
+                    print(f'Downloaded Component: {component_name}')
 
 def pack():
     cur_dir = Path(__file__).parent.absolute()
@@ -42,30 +89,25 @@ def pack():
     if output_packages_dir.exists():
         shutil.rmtree(output_packages_dir)
     output_packages_dir.mkdir(parents=True)
-    
-    external_packages = {'ArtistBioAlternate': 'https://github.com/mayanez/WilB-Biography-Mod/releases/latest/download/SMP-Biography-Mod.zip', 
-                         'ComposerBio': 'https://github.com/mayanez/WilB-Biography-Mod/releases/latest/download/SMP-Biography-Mod.zip'}
+
+    output_components_dir = output_dir/'user-components'
+    if output_components_dir.exists():
+        shutil.rmtree(output_components_dir)
+    output_components_dir.mkdir(parents=True)
 
     for d in (root_dir/'packages').glob('*'):
-        d_name = os.path.basename(d)
-        if d_name not in external_packages.keys():
-            for f in d.glob('*'):
-                package_zip = output_packages_dir/f'{d_name}.zip'
-                with ZipFile(package_zip, 'w', compression=zipfile.ZIP_DEFLATED, compresslevel=9) as z:
-                    zipdir(z, f)   
-                    print(f'Generated package: {package_zip}')
-                
-                # there should be only one package per dir anyway
-                break
-        else:
-            urllib.request.urlretrieve(external_packages[d_name], output_packages_dir/f'{d_name}.zip')
-            print(f'Downloaded package: {d_name}')
+        handle_package(d, output_packages_dir)
+
+    for d in (root_dir/'user-components').glob('*'):
+        handle_user_components(d, output_components_dir)
 
     with ZipFile(output_zip, 'w', compression=zipfile.ZIP_DEFLATED, compresslevel=9) as z:
         zipdir(z, output_packages_dir, 'packages')
+        zipdir(z, output_components_dir, 'user-components')
         zipdir(z, root_dir/'fonts', 'fonts')
         z.write(root_dir/'layout'/'classical.fcl', 'layout/classical.fcl')
         z.write(root_dir/'layout'/'theme.fcl', 'layout/theme.fcl')
+        handle_additional_files(root_dir, z)
 
     print(f'Generated file: {output_zip}')
 
